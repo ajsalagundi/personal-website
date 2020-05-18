7fe899d7e8e2fe6d92fb5bad869e20d67d4d9f0e
Ananth Salagundi <ajsalagundi@gmail.com>
2020-05-18 10:51:25 -0500

    initiating with tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-class App extends React.Component{
-    render() {
-        return(
-          <h1>Hello World</h1>
-        );
-    }
-}
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById("root"));
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      body: ['Josefin Sans', 'sans-serif']
+    },
+    extend: {
+      colors:{
+        orange: '#ee6002'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
